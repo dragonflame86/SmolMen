fe85253699e0d2f459087d0a213efebbe57171c8
Yavanni <thenuclearnexus@gmail.com>
2024-04-21 15:46:51 -0400

    made block outline visible in dark

--- assets/minecraft/shaders/include/darkness.glsl
+++ assets/minecraft/shaders/include/darkness.glsl
@@ -2,13 +2,10 @@
 #define SCALE 10000
 
 vec4 calculate_color(vec4 texelColor, vec4 vertexColor, bool saveTransparency) {
-
-    if (vertexColor.r > MIN_THRESHOLD)
-        return texelColor;
-
     float t = vertexColor.r / MIN_THRESHOLD;
     t = pow(SCALE, t) - 1;
     t /= SCALE - 1;
+    t = clamp(t, 0, 1);
 
     vec4 dark = vec4(0, 0, 0, saveTransparency ? texelColor.a : 1);
     texelColor = mix(dark, texelColor, t);
